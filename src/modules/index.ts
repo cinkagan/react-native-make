ecf480b439cd25c0bdb362c597bf347a34b434bf
Kagan Cin <kcin@thinksmobility.com>
2022-10-05 20:37:29 +0300

    Added option to change app name

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,2 +1,3 @@
 export { setIconCommand } from './setIcon/setIcon.command';
 export { setSplashScreenCommand } from './setSplashScreen/setSplashScreen.command';
+export { setNameCommand } from './setName/setName.command';
--- src/rn-plugin.config.ts
+++ src/rn-plugin.config.ts
@@ -1,8 +1,8 @@
 import { UserDependencyConfig } from '@react-native-community/cli';
-import { setIconCommand, setSplashScreenCommand } from './modules';
+import { setIconCommand, setNameCommand, setSplashScreenCommand } from './modules';
 
 const v2Config: UserDependencyConfig = {
-  commands: [setIconCommand, setSplashScreenCommand],
+  commands: [setIconCommand, setNameCommand, setSplashScreenCommand],
 };
 
 export const rnPluginConfig: any = v2Config;
